20a3ed852abbbefc9c6b4f3dff17a8418f0033d2
vivivargas15 <viviana_vargas15@hotmail.com>
2023-06-09 11:17:22 -0500

    Creacion de vista blog, blog.html, urls ingreso de datos en el panel adm

--- avengersblog/models.py
+++ avengersblog/models.py
@@ -7,6 +7,7 @@ class Post(models.Model):
 	title= models.CharField(max_length=200)
 	text=models.TextField()
 	time=models.CharField(max_length=200)
+	image=models.ImageField(upload_to='blog', null=True, blank=True)
 
 	def __str__(self):
 		return self.title
--- avengersblog/views.py
+++ avengersblog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
+
+def home(request):
+	return render(request, 'avengersblog/home.html')
+
+def blog(request):
+	posts=Post.objects.all()
+	return render(request, 'avengersblog/blog.html', {'posts':posts})
